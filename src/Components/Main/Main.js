3f875d61a507c64733357ea331dc566268ff7928
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-02-16 15:25:31 -0500

    Add first draft of portfolio

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,14 @@
+import "./Main.css"
+import NavBar from "../NavBar/NavBar";
+import InfoText from "../InfoText/InfoText";
+
+
+function Main() {
+  return (
+    <div className="main-container">
+      <InfoText />
+    </div>
+  );
+};
+
+export default Main;
